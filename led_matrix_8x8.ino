0d0de7faef5531c8973c47e17c9757bc38ed9695
Deimos <deimoshall@gmail.com>
2023-02-08 17:44:43 -0600

    first commit

---
+++ led_matrix_8x8.ino
@@ -0,0 +1,23 @@
+#include <FastLED.h>
+
+#define LED_PIN 4
+#define NUM_LEDS 64
+#define COLOR_ORDER GRB
+#define CHIPSET WS2812B
+#define BRIGHTNESS 128
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(100);
+    leds[i] = CRGB::Black;
+  }
+}
